22f1fef764b71e6ebfb8fee9173533971a3b3efd
Michael M. Tiller <michael.tiller@juliahub.com>
2023-11-08 18:09:53 -0500

    chore: a bit of cleanup

--- src/process.jl
+++ src/process.jl
@@ -24,18 +24,7 @@ function process(opts::Options)
                 # If the input file is more recent than the regenerated file or we are forcing 
                 # regeneration, run Literate.jl
                 if fs.mtime > ms.mtime || opts.force
-                    # pat = "$(base)-[0-9]+\\.svg"
-                    # print("Searching for image files that match the pattern: $(pat)")
-                    # rexp = Regex(pat)
-                    # images = filter(x -> occursin(rexp, x), files)
-
-                    # # Remove existing image files
-                    # for image in images
-                    #     println("Removing $(image)")
-                    #     rm(image)
-                    # end
-
-                    println("Generating markdown for $(mfile) from $(ifile) since $(fs.mtime) > $(ms.mtime)")
+                    # println("Generating markdown for $(mfile) from $(ifile) since $(fs.mtime) > $(ms.mtime)")
                     Literate.markdown(ifile, joinpath(opts.output, subdir); flavor=Literate.CommonMarkFlavor(), execute=opts.execute)
                 end
             end
@@ -47,7 +36,7 @@ function process(opts::Options)
 
 
                 if fs.mtime > ns.mtime || opts.force
-                    println("Generating notebook for $(nfile) from $(file) since $(fs.mtime) > $(ns.mtime)")
+                    # println("Generating notebook for $(nfile) from $(file) since $(fs.mtime) > $(ns.mtime)")
                     Literate.notebook(ifile, "."; execute=opts.execute)
                 end
             end
