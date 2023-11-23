1b38e20fa84d02f56e26c354cb9d963bdc5d1d04
Michael M. Tiller <michael.tiller@juliahub.com>
2023-11-23 08:29:45 -0500

    feat: handle exceptions more gracefully

--- src/process.jl
+++ src/process.jl
@@ -7,39 +7,47 @@ function process(opts::Options)
         subdir = relpath(root, opts.docs)
         # Loop over all files found while walking
         for file in files
-            if !endswith(file, opts.ext)
-                continue
-            end
-
-            # Stat the input file
-            ifile = joinpath(root, file)
-            fs = stat(ifile)
-
-            # Get the base name of the .jl file
-            (base,) = splitext(file)
-
-            if opts.markdown
-                mfile = joinpath(opts.output, subdir, "$(base).md")
-                ms = stat(mfile)
-
-                # If the input file is more recent than the regenerated file or we are forcing 
-                # regeneration, run Literate.jl
-                if fs.mtime > ms.mtime || opts.force
-                    # println("Generating markdown for $(mfile) from $(ifile) since $(fs.mtime) > $(ms.mtime)")
-                    Literate.markdown(ifile, joinpath(opts.output, subdir); flavor=Literate.CommonMarkFlavor(), execute=opts.execute)
+            try
+                if !endswith(file, opts.ext)
+                    continue
                 end
-            end
 
-            if opts.notebook
-                # This will be the name of the notebook file
-                mfile = joinpath(opts.output, subdir, "$(base).ipynb")
-                ns = stat(nfile)
+                # Stat the input file
+                ifile = joinpath(root, file)
+                fs = stat(ifile)
 
+                # Get the base name of the .jl file
+                (base,) = splitext(file)
 
-                if fs.mtime > ns.mtime || opts.force
-                    # println("Generating notebook for $(nfile) from $(file) since $(fs.mtime) > $(ns.mtime)")
-                    Literate.notebook(ifile, "."; execute=opts.execute)
+                if opts.markdown
+                    mfile = joinpath(opts.output, subdir, "$(base).md")
+                    ms = stat(mfile)
+
+                    # If the input file is more recent than the regenerated file or we are forcing 
+                    # regeneration, run Literate.jl
+                    if fs.mtime > ms.mtime || opts.force
+                        # println("Generating markdown for $(mfile) from $(ifile) since $(fs.mtime) > $(ms.mtime)")
+                        Literate.markdown(ifile, joinpath(opts.output, subdir); flavor=Literate.CommonMarkFlavor(), execute=opts.execute)
+                    end
                 end
+
+                if opts.notebook
+                    # This will be the name of the notebook file
+                    mfile = joinpath(opts.output, subdir, "$(base).ipynb")
+                    ns = stat(nfile)
+
+
+                    if fs.mtime > ns.mtime || opts.force
+                        # println("Generating notebook for $(nfile) from $(file) since $(fs.mtime) > $(ns.mtime)")
+                        Literate.notebook(ifile, "."; execute=opts.execute)
+                    end
+                end
+            catch e
+                printstyled("Error "; color=:red)
+                print("processing file ")
+                printstyled(file; color=:blue)
+                println();
+                Base.display_error(e)
             end
         end
     end
